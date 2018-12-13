c38943c9cd65243c8bed112a541e324a68200688
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-12-12 21:57:14 -0500

    Validating try/catch around await

---
+++ async functions/exception.js
@@ -0,0 +1,27 @@
+'use strict'
+
+async function generateException () {
+  throw new Error("fail")
+}
+
+async function generateRejected () {
+  return Promise.reject(new Error("fail"))
+}
+
+async function main () {
+
+  try {
+    await generateException()
+  } catch (e) {
+    console.log("Exception caught: " + e)
+  }
+
+  try {
+    await generateException()
+  } catch (e) {
+    console.log("Rejection caught: " + e)
+  }
+
+}
+
+main();
