53ae350254a97a39ca0ac20b2626fa6b952134c1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-05 12:58:44 -0400

    ES5 version

---
+++ handbook/hello world.es5.js
@@ -0,0 +1,13 @@
+var http = require('http')
+var hostname = '127.0.0.1'
+var port = 3000
+// Callback is triggered for every request
+const server = http.createServer(function (request, response) {
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/plain')
+  response.end('Hello World\n')
+})
+// Callback is triggered when server is ready
+server.listen(port, hostname, function () {
+  console.log("Server running at http://" + hostname + ":" + port + "/")
+})
