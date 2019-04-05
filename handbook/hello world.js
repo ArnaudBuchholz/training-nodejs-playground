30b775e35f9407f9547534d9ec9dc41b62f5f9e5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-05 12:58:38 -0400

    Adds comment

--- handbook/hello world.js
+++ handbook/hello world.js
@@ -1,13 +1,13 @@
 const http = require('http')
 const hostname = '127.0.0.1'
 const port = 3000
-
+// Callback is triggered for every request
 const server = http.createServer((request, response) => {
   response.statusCode = 200
   response.setHeader('Content-Type', 'text/plain')
   response.end('Hello World\n')
 })
-
+// Callback is triggered when server is ready
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
