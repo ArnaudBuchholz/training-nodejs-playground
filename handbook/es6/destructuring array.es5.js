ba62c1a0c3f19d3af811d9a5f59b3a4de34c3b8a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-06 11:19:03 -0400

    Fix sample

--- handbook/es6/destructuring array.es5.js
+++ handbook/es6/destructuring array.es5.js
@@ -1,7 +1,7 @@
 var fibonacci = [1, 2, 3, 5, 8]
 var first = fibonacci[0]
 var second = fibonacci[1]
-var res = fibonacci.slice(2)
+var rest = fibonacci.slice(2)
 console.log(first) // 1
 console.log(second) // 2
 console.log(rest) // [3, 5, 8]
