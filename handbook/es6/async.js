7c3b916c025d65af52a7bd09ead13eed1d7968cb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-06 11:07:27 -0400

    Better sample

--- handbook/es6/async.js
+++ handbook/es6/async.js
@@ -1,14 +1,11 @@
-function getResultAfter1second () {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved')
-    }, 1000)
-  })
-}
+const getResultAfter1second = () => new Promise(resolve => {
+  setTimeout(() => resolve('resolved'), 1000)
+})
 
 async function asyncCall () {
   console.log('calling')
   console.log(await getResultAfter1second()) // resolved
+  return 'done'
 }
 
-asyncCall()
+asyncCall().then(message => console.log(message))
