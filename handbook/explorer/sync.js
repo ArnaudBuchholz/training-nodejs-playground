608ab4ce7d04f13563bb846926562c7b12fc527c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-08 13:18:39 -0400

    Simplification of the synchronous example

--- handbook/explorer/sync.js
+++ handbook/explorer/sync.js
@@ -2,40 +2,14 @@ const server = require('./server')
 const fs = require('fs')
 const path = require('path')
 
-function safeStatSync (requestedPath) {
-  try {
-    return fs.statSync(requestedPath)
-  } catch (e) {
-    console.error(requestedPath, e)
-  }
-}
-
-// Return true if folder
-function writeInfo (response, requestedPath, requestedStat) {
-  const name = path.basename(requestedPath)
-  if (!requestedStat) {
-    response.write(`${name} ?\n`)
-    return false
-  }
-  if (requestedStat.isDirectory()) {
-    response.write(`${name}\\\n`)
-    return true
-  }
-  response.write(`${name.padEnd(40, '.')} ${requestedStat.size.toString().padStart(10, ' ')}\n`)
-  return false
-}
-
-server.run((requestedPath, response) => {
-  response.statusCode = 200
-  response.setHeader('Content-Type', 'text/plain')
-  response.write(requestedPath)
-  response.write('\n')
-  const requestedStat = safeStatSync(requestedPath)
-  if (writeInfo(response, requestedPath, requestedStat)) {
-    fs.readdirSync(requestedPath).forEach(name => {
-      const subPath = path.join(requestedPath, name)
-      writeInfo(response, subPath, safeStatSync(subPath))
-    })
-  }
-  response.end()
+server.run((folderPath, output) => {
+  fs.readdirSync(folderPath).forEach(name => {
+    const subPath = path.join(folderPath, name)
+    try {
+      const stat = fs.statSync(subPath)
+      output(subPath, stat)
+    } catch (e) {
+      output(subPath, {error: e})
+    }
+  })
 })
