171c9abc6dc8c6347d8bcbe44a3a0308aa39225a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-10 15:30:32 -0400

    Typo in documentation

--- handbook/explorer/sample.js
+++ handbook/explorer/sample.js
@@ -1,8 +1,8 @@
 const server = require('./server')
 
 server.run((filePath, output, end) => {
-  // output HTML will filePath and file information
-  // (isDirectory() = true / false, size gives file size)
+  // output HTML with file path and information
+  // (isDirectory() = true / false, size = file size)
   output(filePath, {
     isDirectory: () => false,
     size: 1
