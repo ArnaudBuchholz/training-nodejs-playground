9237b422cbfe4efcf38745f661456c1ebd2b5fc9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-10 15:27:55 -0400

    Rename and document

--- handbook/explorer/sample.js
+++ handbook/explorer/sample.js
@@ -1,9 +1,11 @@
 const server = require('./server')
 
 server.run((filePath, output, end) => {
+  // output HTML will filePath and file information
+  // (isDirectory() = true / false, size gives file size)
   output(filePath, {
     isDirectory: () => false,
     size: 1
   })
-  end()
+  end() // call when done
 })
