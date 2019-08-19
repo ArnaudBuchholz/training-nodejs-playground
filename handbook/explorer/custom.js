d35e11b49b0b5f49fff06bf3ac68b09d7d51e89b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-19 00:29:17 -0400

    WIP

--- handbook/explorer/custom.js
+++ handbook/explorer/custom.js
@@ -2,6 +2,10 @@
 
 module.exports = callback => {
   return function (request, response, folderPath) {
-    
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    response.write('[')
+    callback(folderPath, , () => response.end(']'))
   }
 }
