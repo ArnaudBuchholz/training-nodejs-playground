c9061ba394ef6f9905e36f4f496283e200814b83
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-19 13:26:30 -0400

    Sync implementation

--- handbook/explorer/sync.js
+++ handbook/explorer/sync.js
@@ -4,7 +4,16 @@ const fs = require('fs')
 const path = require('path')
 
 function sync (folderPath, output, end) {
-
+  fs.readdirSync(folderPath).forEach(name => {
+    const subPath = path.join(folderPath, name)
+    try {
+      const stat = fs.statSync(subPath)
+      output(subPath, stat)
+    } catch (e) {
+      output(subPath, { error: e })
+    }
+  })
+  end()
 }
 
 module.exports = require('./custom')(sync)
