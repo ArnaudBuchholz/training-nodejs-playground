53100c96679d35d471ad8db67a3bbe32c2c651f2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-20 13:20:34 -0400

    Rename to exercise

---
+++ handbook/explorer/exercise.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Output item info.
+ *
+ * @callback api.output
+ *
+ * @param {String} name Name of the folder item
+ * @param {fs.Stats} file Folder item statistics (https://nodejs.org/api/fs.html#fs_class_fs_stats)
+ */
+
+/**
+ * End output.
+ *
+ * @callback api.end
+ */
+
+/**
+ *
+ *
+ * @param {String} folderPath Full path of the folder to explore
+ * @param {api.output} output
+ * @param {api.end} end
+ */
+module.exports = (filePath, output, end) => {
+
+  end()
+}
