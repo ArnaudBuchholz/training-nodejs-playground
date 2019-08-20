339aec0f8a516ffdf53ff2d0aec708ef6a6287aa
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-20 13:18:25 -0400

    Rename and adapt to new api

--- handbook/explorer/folderStatSync.js
+++ handbook/explorer/folderStatSync.js
@@ -1,7 +1,6 @@
-const server = require('./server')
 const fs = require('fs')
 
-server.run((folderPath, output, end) => {
+module.exports = (folderPath, output, end) => {
   output(folderPath, fs.statSync(folderPath))
   end()
-})
+}
