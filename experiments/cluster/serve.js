fe9b4e0fe9e2cf14b908e49fd1be751a96ad7e01
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-11 14:58:07 -0400

    Simple server

---
+++ experiments/cluster/serve.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { log, serve } = require('reserve')
+
+module.exports = () => log(serve({
+  port: 8080,
+  mappings: [{
+    match: '^/$',
+    custom: (request, response) => new Promise(resolve => {
+      response.writeHead(200, {
+        'content-type': 'text/plain'
+      })
+      response.write('begin\n')
+      setTimeout(() => {
+        response.end('end')
+        resolve()
+      }, 1000)
+    })
+  }]
+}))
